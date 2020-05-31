f6821b95042ab481f8dda650657a0a74e8638a63
Sudarsan <sudarsangp@gmail.com>
2020-05-31 14:12:36 +0800

    update packages and fix vulnerabilities

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,16 +1,17 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
-const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
 const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CopyWebpackPlugin = require('copy-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'production',
   optimization: {
+    minimize: true,
     minimizer: [
-      new UglifyJsPlugin({
+      new TerserPlugin({
         cache: true,
         parallel: true
       }),
@@ -21,14 +22,16 @@ module.exports = merge(common, {
     }
   },
   plugins: [
-    new CleanWebpackPlugin(['dist']),
+    new CleanWebpackPlugin(),
     new MiniCssExtractPlugin({
       filename: "[name].[hash].css",
       chunkFilename: "[id].[hash].css"
     }),
-    new CopyWebpackPlugin([
-      {from: 'manifest.json', to: 'manifest.json'}
-    ])
+    new CopyWebpackPlugin({
+      patterns: [
+        {from: 'manifest.json', to: 'manifest.json'}
+      ]
+    })
   ],
   module: {
     rules: [
